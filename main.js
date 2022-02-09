1534dcdd05a553dc5d34cb1a94a2e740f2dfda8c
Zharkevich Ilya <zharkevichilya@gmail.com>
2022-02-09 16:13:39 +0300

    feat: end second day's homework

--- main.js
+++ main.js
@@ -18,17 +18,33 @@ const keys = {
 const setting = {
   start: false,
   score: 0,
-  speed: 3
+  speed: 3,
+  traffic: 2.5
 };
+
+function getQuantityElements(heightElement) {
+  return document.documentElement.clientHeight / heightElement + 1;
+}
 
 function startGame() {  
   start.classList.add('hide');
 
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < getQuantityElements(100); i++) {
     const line = document.createElement('div');
     line.classList.add('line');
-    line.style.top = (i * 75) + 'px';
+    line.style.top = (i * 100) + 'px';
+    line.y = i * 100;
     gameArea.appendChild(line);
+  }
+
+  for (let i = 0; i < getQuantityElements(100 * setting.traffic); i++) {
+    const enemy = document.createElement('div');
+    enemy.classList.add('enemy');
+    enemy.y = -100 * setting.traffic * (i + 1);
+    enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+    enemy.style.top = enemy.y + 'px';
+    enemy.style.background = 'transparent url(./img/enemy.png) center / cover no-repeat';
+    gameArea.appendChild(enemy);
   }
 
   setting.start = true;
@@ -39,8 +55,11 @@ function startGame() {
 }
 
 function playGame() {
-  console.log('Play game!');
+
   if(setting.start) {
+    moveRoad();
+    moveEnemy();
+
     if(keys.ArrowLeft && setting.x > 0) {
       setting.x -= setting.speed;
     }
@@ -62,6 +81,7 @@ function playGame() {
 
     requestAnimationFrame(playGame);
   }
+
 }
 
 function startRun(event) {
@@ -72,4 +92,31 @@ function startRun(event) {
 function stopRun(event) {
   event.preventDefault();
   keys[event.key] = false;
+}
+
+function moveRoad() {
+  let lines = document.querySelectorAll('.line');
+  lines.forEach(function(line) {
+    line.y += setting.speed;
+    line.style.top = line.y + 'px';
+
+    if(line.y >= document.documentElement.clientHeight) {
+      line.y = -100;
+    }
+
+  });
+}
+
+function moveEnemy() {
+  let enemy = document.querySelectorAll('.enemy');
+  enemy.forEach(function(item){
+    item.y += setting.speed / 2;
+    item.style.top = item.y + 'px';
+
+    if (item.y >= document.documentElement.clientHeight) {
+      item.y = -100 * setting.traffic;
+      item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+    }
+
+  });
 }
